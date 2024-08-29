4b7404be33acc12a0b80944a4ddc7e52727bb3bb
space-commits <57108281+space-commits@users.noreply.github.com>
2024-08-29 14:04:40 +0100

    set up page components, css tweaks

---
+++ SiliconRockWebsite/Classes/Utils.cs
@@ -0,0 +1,10 @@
+﻿namespace SiliconRockWebsite.Classes
+{
+    public static class Utils
+    {
+        public static async Task<string[]> InitializeStringsAsync(string text)
+        {
+            return await Task.Run(() => text.Split('|'));
+        }
+    }
+}
